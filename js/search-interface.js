56b32fe06340b2d3290d2079e9f29bce44243d5b
stella huayhuaca <luces.huayhuaca@gmail.com>
2017-03-17 16:38:33 -0700

    refactor

--- js/search-interface.js
+++ js/search-interface.js
@@ -1,19 +1,13 @@
+//require object-backend:
 var Issue = require('./../js/search.js').issueModule;
-
-var displayDoctors = function(outputs){
-  // $('#practicesNames').empty();
-  // $('#descriptions').empty();
-  for (var i = 0; i < outputs.length; i++) {
-    $('#result').append('<h3> '+ outputs[i].name+ ' </h3>'+ '<p> '+outputs[i].description+' </p>'+'<p>'+ outputs[i].bio+'</p>'+'<img src="'+outputs[i].image+'"/>');
-  }
-};
 
 $(document).ready(function() {
   $('#intake-form').submit(function(event) {
     event.preventDefault();
     var userInput= $('#issue').val();
-    var newIssue = new Issue();
-    newIssue.getDoctors(userInput, displayDoctors);
+    var newIssue = new Issue(userInput);
+    //Call prototype method on instantiated object:
+    newIssue.getDoctors();
     $('#result').show();
   });
 });
--- js/search.js
+++ js/search.js
@@ -1,33 +1,46 @@
 var apiKey = require('./../.env').apiKey;
-function Issue()
+// Create object and his property:
+function Issue(userInput)
 {
+  this.userInput= userInput;
 }
 
-Issue.prototype.getDoctors = function(userInput, callback) {
+//Create prototype method to manipulate API data:
+Issue.prototype.getDoctors = function() {
   var outputs=[];
+
   var practicesNames= [];
-  var descriptions=[];
+  var phones=[];
   var images=[];
   var bios=[];
-
-  $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+userInput+'&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=8&user_key='+apiKey)
-
+//call API data with get method:
+  $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+this.userInput+'&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=8&user_key='+apiKey)
+//then method to push organized,selected data into "outputs" array:
   .then(function(result){
     console.log(result);
     // console.log(JSON.stringify(result));
     for(var i=0; i< result.data.length; i++){
       practicesNames.push(result.data[i].practices[0].name);
-      descriptions.push(result.data[i].practices[0].description);
+      phones.push(result.data[i].practices[0].phones[0].number);
       images.push(result.data[i].profile.image_url);
       bios.push(result.data[i].profile.bio);
-      var output= {name: practicesNames[i], description: descriptions[i], image:images[i], bio:bios[i]};
+      var output= {name: practicesNames[i], phone: phones[i], image:images[i], bio:bios[i]};
       outputs.push(output);
     }
-    callback(outputs);
+    displayDoctors(outputs);
   })
+
   .fail(function(error){
     console.log("fail");
   });
 };
 
+//Create method to display data:
+var displayDoctors = function(outputs){
+  for (var i = 0; i < outputs.length; i++) {
+    $('#result').append('<h3> '+ outputs[i].name+ ' </h3>'+ '<p> Phone Number: '+outputs[i].phone+' </p>'+'<p>'+ outputs[i].bio+'</p>'+'<img src="'+outputs[i].image+'"/>');
+  }
+};
+
+//export object:
 exports.issueModule = Issue;
