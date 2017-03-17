44c3e754608dbf3403dd9a958c129b8b872581be
stella huayhuaca <luces.huayhuaca@gmail.com>
2017-03-17 15:06:12 -0700

    display doctor bio and picture

--- js/search-interface.js
+++ js/search-interface.js
@@ -1,9 +1,10 @@
 var Issue = require('./../js/search.js').issueModule;
 
-var displayDoctors = function(practicesNames){
-  $('#practicesNames').empty();
-  for (var i = 0; i < practicesNames.length; i++) {
-    $('#practicesNames').append('<li> '+ practicesNames[i]+ ' </li>');
+var displayDoctors = function(outputs){
+  // $('#practicesNames').empty();
+  // $('#descriptions').empty();
+  for (var i = 0; i < outputs.length; i++) {
+    $('#result').append('<h3> '+ outputs[i].name+ ' </h3>'+ '<p> '+outputs[i].description+' </p>'+'<p>'+ outputs[i].bio+'</p>'+'<img src="'+outputs[i].image+'"/>');
   }
 };
 
--- js/search.js
+++ js/search.js
@@ -4,7 +4,11 @@ function Issue()
 }
 
 Issue.prototype.getDoctors = function(userInput, callback) {
+  var outputs=[];
   var practicesNames= [];
+  var descriptions=[];
+  var images=[];
+  var bios=[];
 
   $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+userInput+'&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=8&user_key='+apiKey)
 
@@ -13,8 +17,13 @@ Issue.prototype.getDoctors = function(userInput, callback) {
     // console.log(JSON.stringify(result));
     for(var i=0; i< result.data.length; i++){
       practicesNames.push(result.data[i].practices[0].name);
+      descriptions.push(result.data[i].practices[0].description);
+      images.push(result.data[i].profile.image_url);
+      bios.push(result.data[i].profile.bio);
+      var output= {name: practicesNames[i], description: descriptions[i], image:images[i], bio:bios[i]};
+      outputs.push(output);
     }
-    callback(practicesNames);
+    callback(outputs);
   })
   .fail(function(error){
     console.log("fail");
